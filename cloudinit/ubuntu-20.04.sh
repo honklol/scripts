2d9a77212972b3ea5b1d0e7cc865908d76f873c7
honk <honklol@protonmail.com>
2022-09-04 16:25:11 +0200

    Update ubuntu-20.04.sh

--- cloudinit/ubuntu-20.04.sh
+++ cloudinit/ubuntu-20.04.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 if [ -z $1 ]; then
-echo "Error: No storage medium provided.
-exit
+echo "Error: No storage medium provided."
+exit 1
 else
 curl -LO https://cloud-images.ubuntu.com/focal/current/focal-server-cloudimg-amd64.img
 qm create 100000 --memory 2048 --name ubuntu-20.04-cloudinit --net0 virtio,bridge=vmbr1
@@ -14,4 +14,5 @@ qm set 100000 --ide2 local:cloudinit
 qm set 100000 --boot c --bootdisk scsi0
 qm set 100000 --serial0 socket --vga serial0
 rm focal-server-cloudimg-amd64.img
+exit 0
 fi
