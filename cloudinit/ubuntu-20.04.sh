ed26309115de801a66364aa1c80df4b72e774bff
honk <github@honk.email>
2023-08-21 13:10:00 +0200

    fix: use user provided storage rather than local

--- cloudinit/ubuntu-20.04.sh
+++ cloudinit/ubuntu-20.04.sh
@@ -10,7 +10,7 @@ curl -LO https://cloud-images.ubuntu.com/focal/current/focal-server-cloudimg-amd
 qm create $2 --memory 2048 --name ubuntu-20.04-cloudinit --net0 virtio,bridge=vmbr1
 qm importdisk $2 focal-server-cloudimg-amd64.img $1
 qm set $2 --scsihw virtio-scsi-pci --scsi0 $1:$2/vm-$2-disk-0.raw
-qm set $2 --ide2 local:cloudinit
+qm set $2 --ide2 $1:cloudinit
 qm set $2 --boot c --bootdisk scsi0
 qm set $2 --serial0 socket --vga serial0
 qm template $2
