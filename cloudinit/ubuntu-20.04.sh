154944480616b9d353e98b9bd94bac0e0cc1ba00
honk <hello@honk.one>
2024-06-22 13:55:47 +0200

    fix: allow specifying a network bridge

--- cloudinit/ubuntu-20.04.sh
+++ cloudinit/ubuntu-20.04.sh
@@ -5,9 +5,12 @@ exit 1
 elif [ -z $2 ]; then
 echo "Error: No VM ID provided."
 exit 1
+elif [ -z $3 ]; then
+echo "Error: No network bridge provided."
+exit 1
 else
 curl -LO https://cloud-images.ubuntu.com/focal/current/focal-server-cloudimg-amd64.img 
-qm create $2 --memory 2048 --name ubuntu-20.04-cloudinit --net0 virtio,bridge=vmbr1
+qm create $2 --memory 2048 --name ubuntu-20.04-cloudinit --net0 virtio,bridge=$3
 qm importdisk $2 focal-server-cloudimg-amd64.img $1
 qm set $2 --scsihw virtio-scsi-pci --scsi0 $1:$2/vm-$2-disk-0.raw
 qm set $2 --ide2 $1:cloudinit
